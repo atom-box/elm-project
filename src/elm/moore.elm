7e1d1263ba2ec8d851e4c14d618be8537d4117a4
Evan Genest <genest.1@osu.edu>
2019-03-09 16:50:17 -0600

    Add a pipe to the toString. Add a pipe to the adder.

--- src/elm/moore.elm
+++ src/elm/moore.elm
@@ -1,4 +1,13 @@
 --- main is a value, not a funct.
 module Main exposing(..)
+import Html
+
+add a b =
+  a + b
+counter =
+  add 7 <|
+  add 13 300
+
+--- main should be set to SVG, Html, or Program (all are types)
 main  =
-  "Hello World"
+  Html.text <|  String.fromInt counter 
